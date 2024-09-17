8ef32e7c3af770ce373cb28bb2b5cb4ffaa76b35
IbrahemHassen <ibrahemmohye@gmail.com>
2024-09-17 20:06:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,19 +10,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
-
-
-# Setting page configuration
-st.set_page_config(page_title="Aviation flights fare", page_icon="✈️", layout='wide')
-
-# Loading data
-df = pd.read_csv('cleaned_df.csv')
-
-with st.sidebar:
-
-    st.sidebar.image('R.jpg')
-    st.sidebar.subheader("This dashboard for Indian Aviation Flights Fare aimed at predicting the prices of flight tickets")
-    st.sidebar.write("")
     
 
 
