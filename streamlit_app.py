9e966f44b6f6cc8b66f6692d368bc7707c0e0567
IbrahemHassen <ibrahemmohye@gmail.com>
2024-09-17 15:43:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,32 +24,8 @@ with st.sidebar:
     st.sidebar.subheader("This dashboard for Indian Aviation Flights Fare aimed at predicting the prices of flight tickets")
     st.sidebar.write("")
     
-    data = df.copy()
-    source = st.sidebar.selectbox("Departure City", ['All'] + list(data['Source'].unique()))
-    # data after Source selection
-    if source != 'All':
-        data = data[data['Source'] == source]
-    
-    destination = st.sidebar.selectbox("Arrival City", ['All'] + list(data['Destination'].unique()))
-    # data after Source and Destination selection
-    if destination != 'All':
-        data = data[data['Destination'] == destination]
-
-    duration = data[(data['Source'] == source) & (data['Destination'] == destination)]
-    
-    airline = st.sidebar.selectbox("Airline Carrier", ['All'] + list(data['Airline'].unique()))
-    # data after Source and Destination and Month and Day selection and departure hour selection and airline selection
-    if airline != 'All':
-        data = data[data['Airline'] == airline]
-
-    add_info = st.sidebar.selectbox("Additional Services", ['All'] + list(data['Additional_Info'].unique()))
-
-    filter_box = st.sidebar.selectbox("Filter Prices on", [None, 'Day', 'Month', 'Dep_Hour'])
 
 
-
-    st.sidebar.write("")
-    st.sidebar.markdown("Made by [Ibrahem Mohye]")
    
 
 
