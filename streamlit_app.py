310623d54980a4d9484c59ee3735152a55772c5b
IbrahemHassen <ibrahemmohye@gmail.com>
2024-09-17 15:26:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ with st.sidebar:
 
 
     st.sidebar.write("")
-    st.sidebar.markdown("Made by [Ibrahem Mohye]
+    st.sidebar.markdown("Made by [Ibrahem Mohye]")
 
     # filtering Function
 def filter(airline, source, destination, add_info):
